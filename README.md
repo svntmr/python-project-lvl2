ba27d0b7bb9816a8c6087e272e94f1227733946b
Savin Timur <svn.tmr@bk.ru>
2022-02-06 15:23:04 +0300

    add pre-commit hooks, add pre-commit GitHub action
    update project dependencies list

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/svntmr/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/svntmr/python-project-lvl2/actions)
+[![Linter status](https://github.com/svntmr/python-project-lvl2/actions/workflows/make-lint.yml/badge.svg)](https://github.com/svntmr/python-project-lvl2/actions)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,19 +4,19 @@ import argparse
 
 def main():
     """Start cli tool instance."""
-    gendiff = argparse.ArgumentParser(description='Generate diff')
-    gendiff.add_argument('first_file', type=str)
-    gendiff.add_argument('second_file', type=str)
+    gendiff = argparse.ArgumentParser(description="Generate diff")
+    gendiff.add_argument("first_file", type=str)
+    gendiff.add_argument("second_file", type=str)
     gendiff.add_argument(
-        '-f',
-        '--f',
-        metavar='FORMAT',
+        "-f",
+        "--f",
+        metavar="FORMAT",
         type=str,
-        help='set format of output',
+        help="set format of output",
     )
     args = gendiff.parse_args()
     print(args.accumulate(args.integers))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
