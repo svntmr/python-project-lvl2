cb664cd8d1f55819deb70d4bf0c2a93a51a82072
Savin Timur <svn.tmr@bk.ru>
2022-02-12 18:27:30 +0300

    step 6: refactor test_generate_diff.py

--- tests/gendiff/test_generate_diff.py
+++ tests/gendiff/test_generate_diff.py
@@ -9,21 +9,23 @@ class TestGenerateDiffPlainFiles:
     base_folder: str = f"{TESTS_PATH}/gendiff/fixtures/plain"
 
     base_json_file_path: str = f"{base_folder}/base.json"
-    missing_json_file_path: str = f"{base_folder}/missing.json"
-    added_json_file_path: str = f"{base_folder}/added.json"
-    changed_json_file_path: str = f"{base_folder}/changed.json"
-    combined_json_file_path: str = f"{base_folder}/file2.json"
-
     base_yaml_file_path: str = f"{base_folder}/base.yml"
-    missing_yaml_file_path: str = f"{base_folder}/missing.yml"
-    added_yaml_file_path: str = f"{base_folder}/added.yml"
-    changed_yaml_file_path: str = f"{base_folder}/changed.yaml"
-    combined_yaml_file_path: str = f"{base_folder}/file2.yml"
-
     identical_diff_path: str = f"{base_folder}/diffs/identical"
+
+    missing_json_file_path: str = f"{base_folder}/missing.json"
+    missing_yaml_file_path: str = f"{base_folder}/missing.yml"
     missing_diff_path: str = f"{base_folder}/diffs/missing"
+
+    added_json_file_path: str = f"{base_folder}/added.json"
+    added_yaml_file_path: str = f"{base_folder}/added.yml"
     added_diff_path: str = f"{base_folder}/diffs/added"
+
+    changed_json_file_path: str = f"{base_folder}/changed.json"
+    changed_yaml_file_path: str = f"{base_folder}/changed.yaml"
     changed_diff_path: str = f"{base_folder}/diffs/changed"
+
+    combined_json_file_path: str = f"{base_folder}/file2.json"
+    combined_yaml_file_path: str = f"{base_folder}/file2.yml"
     combined_diff_path: str = f"{base_folder}/diffs/full"
 
     @pytest.mark.parametrize(
@@ -36,34 +38,16 @@ class TestGenerateDiffPlainFiles:
                 id="identical json files diff",
             ),
             pytest.param(
-                base_json_file_path,
-                missing_json_file_path,
-                missing_diff_path,
-                id="missing json files diff",
-            ),
-            pytest.param(
-                base_json_file_path,
-                added_json_file_path,
-                added_diff_path,
-                id="added json files diff",
-            ),
-            pytest.param(
-                base_json_file_path,
-                changed_json_file_path,
-                changed_diff_path,
-                id="changed json files diff",
-            ),
-            pytest.param(
-                base_json_file_path,
-                combined_json_file_path,
-                combined_diff_path,
-                id="combined json files diff",
-            ),
-            pytest.param(
                 base_yaml_file_path,
                 base_yaml_file_path,
                 identical_diff_path,
                 id="identical yaml files diff",
+            ),
+            pytest.param(
+                base_json_file_path,
+                missing_json_file_path,
+                missing_diff_path,
+                id="missing json files diff",
             ),
             pytest.param(
                 base_yaml_file_path,
@@ -72,16 +56,34 @@ class TestGenerateDiffPlainFiles:
                 id="missing yaml files diff",
             ),
             pytest.param(
+                base_json_file_path,
+                added_json_file_path,
+                added_diff_path,
+                id="added json files diff",
+            ),
+            pytest.param(
                 base_yaml_file_path,
                 added_yaml_file_path,
                 added_diff_path,
                 id="added yaml files diff",
             ),
             pytest.param(
+                base_json_file_path,
+                changed_json_file_path,
+                changed_diff_path,
+                id="changed json files diff",
+            ),
+            pytest.param(
                 base_yaml_file_path,
                 changed_yaml_file_path,
                 changed_diff_path,
                 id="changed yaml files diff",
+            ),
+            pytest.param(
+                base_json_file_path,
+                combined_json_file_path,
+                combined_diff_path,
+                id="combined json files diff",
             ),
             pytest.param(
                 base_yaml_file_path,
@@ -99,4 +101,4 @@ class TestGenerateDiffPlainFiles:
     ):
         diff = generate_diff(original_file, changed_file)
         awaited_diff = Path(awaited_diff_file).read_text()
-        assert awaited_diff == diff
+        assert diff == awaited_diff
